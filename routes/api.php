01c1d4d8575285e964c5a1deb3a8a24abb506c79
behaver <qianxing@yeah.net>
2020-11-14 04:55:23 +0800

    feat(api route and http resources): create all http resources and define all api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\Tab as TabResource;
+use App\Http\Resources\Menu as MenuResource;
+use App\Http\Resources\Section as SectionResource;
+use App\Models\Tab;
+use App\Models\Menu;
+use App\Models\Section;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +22,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// request all tabs data
+Route::get('/tabs', function () {
+    return TabResource::collection(Tab::all());
+});
+
+// request all menus data which belong to the assigned tab 
+Route::get('/menus', function (Request $request) {
+    return MenuResource::collection(Menu::all()->where('tab_id', $request->tab_id));
+});
+
+// request all sections data which belong to the assigned menu 
+Route::get('/sections', function (Request $request) {
+    return SectionResource::collection(Section::all()->where('menu_id', $request->menu_id));
+});
+
+// request all sections data which belong to the assigned menu 
+Route::get('/cells', function (Request $request) {
+    return CellResource::collection(Cell::all()->where('folder_id', $request->folder_id)->simplePaginate(15));
 });
